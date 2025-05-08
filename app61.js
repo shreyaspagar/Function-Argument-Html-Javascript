13f19fe155c8693adbddbdd68b6f2fa518f8b9c4
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-05-08 13:02:25 +0530

    Add files via upload

---
+++ app61.js
@@ -0,0 +1,15 @@
+function printInfo(name, age) {
+    console.log(`${name}'s age is ${age}`);
+}
+
+printInfo("Shreyas", 20);
+printInfo("Rajat", 28);
+printInfo(15);
+
+function sum(a , b) {
+    console.log(a + b);
+}
+
+sum(1, 2);
+sum(4, 8);
+sum(45,89);
